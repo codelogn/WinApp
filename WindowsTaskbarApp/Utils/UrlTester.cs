331b686902ac0171c5e593d835f26ac3f90d4199
Robiul Islam <md.robiul@gmail.com>
2025-04-20 22:42:17 -0500

    Fixed url test form

--- WindowsTaskbarApp/Utils/UrlTester.cs
+++ WindowsTaskbarApp/Utils/UrlTester.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing; // Add this for ContentAlignment, Font, and FontStyle
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,8 +19,42 @@ namespace WindowsTaskbarApp.Utils
             try
             {
                 using var httpClient = new HttpClient();
+
+                // Create a new form with a WebView2 control to render the HTML
+                var browserForm = new Form
+                {
+                    Text = $"HTML Viewer - {url}",
+                    Size = new System.Drawing.Size(800, 600)
+                };
+
+                // Add a loading label
+                var loadingLabel = new Label
+                {
+                    Text = "Loading...",
+                    Dock = DockStyle.Fill,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Font = new Font("Arial", 16, FontStyle.Bold)
+                };
+                browserForm.Controls.Add(loadingLabel);
+
+                // Show the form before starting the fetch
+                browserForm.Show();
+
+                // Fetch the HTML content
                 var response = await httpClient.GetStringAsync(url);
-                MessageBox.Show($"Response from {url}:\n{response}", "HTTP Response", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Remove the loading label and add the WebView2 control
+                browserForm.Controls.Clear();
+
+                var webView = new Microsoft.Web.WebView2.WinForms.WebView2
+                {
+                    Dock = DockStyle.Fill
+                };
+
+                await webView.EnsureCoreWebView2Async();
+                webView.NavigateToString(response); // Set the HTML content
+
+                browserForm.Controls.Add(webView);
             }
             catch (Exception ex)
             {
